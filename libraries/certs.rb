17bc7cf5f38dbe40df6347b6e3eec87106814a73
Brett Campbell <invsblduck@gmail.com>
2014-04-23 18:14:44 -0700

    Convert library class to module
    Mixin to Chef::Recipe

--- libraries/certs.rb
+++ libraries/certs.rb
@@ -1,25 +1,29 @@
-class Chef::Recipe::SSL
-  def self.get_cert_paths(server, service, nodeish=nil)
-    nodeish = node unless nodeish
-    paths = { 'pkey'=>nil, 'cert'=>nil, 'cacert'=>nil }
-    
-    paths.keys.each do |key|
-      if nodeish.has_key?(server) and
-         nodeish[server].has_key?('services') and
-         nodeish[server]['services'].has_key?(service) and
-         nodeish[server]['services'][service].has_key?(key)
+module RCB
+  module SSL
+    def get_cert_paths(server, service, nodeish=nil)
+      nodeish = node unless nodeish
+      paths = { 'pkey'=>nil, 'cert'=>nil, 'cacert'=>nil }
 
-        Chef::Log.debug("SSL#get_cert_paths: using node override for #{key}")
-        paths[key] = nodeish[server]['services'][service][key]
+      paths.keys.each do |key|
+        if nodeish.has_key?(server) and
+          nodeish[server].has_key?('services') and
+          nodeish[server]['services'].has_key?(service) and
+          nodeish[server]['services'][service].has_key?(key)
 
-      elsif nodeish['ssl'].has_key?("global_#{key}")
-        Chef::Log.debug("SSL#get_cert_paths: using global attr for #{key}")
-        paths[key] = nodeish['ssl']["global_#{key}"]
-      else
-        Chef::Log.debug("SSL#get_cert_paths: using default attr for #{key}")
-        paths[key] = nodeish['ssl']["default_#{key}"]
+          Chef::Log.debug("SSL#get_cert_paths: using node override for #{key}")
+          paths[key] = nodeish[server]['services'][service][key]
+
+        elsif nodeish['ssl'].has_key?("global_#{key}")
+          Chef::Log.debug("SSL#get_cert_paths: using global attr for #{key}")
+          paths[key] = nodeish['ssl']["global_#{key}"]
+        else
+          Chef::Log.debug("SSL#get_cert_paths: using default attr for #{key}")
+          paths[key] = nodeish['ssl']["default_#{key}"]
+        end
       end
+      paths
     end
-    paths
   end
 end
+
+::Chef::Recipe.send(:include, RCB::SSL)
